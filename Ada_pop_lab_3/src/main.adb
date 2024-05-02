bef90629dad5601b176e515fa14c5477b7a09094
kingmidas1121 <95761121+kingmidas1121@users.noreply.github.com>
2024-05-02 09:58:08 +0300

    Update main.adb

--- Ada_pop_lab_3/src/main.adb
+++ Ada_pop_lab_3/src/main.adb
@@ -10,44 +10,37 @@ procedure Main is
    package String_Lists is new Indefinite_Doubly_Linked_Lists (String);
    use String_Lists;
 
-   WorkTarget : Integer := 10;
-   ProducersCount : Integer := 3;
-   ConsumersCount : Integer := 6;
-   StorageSize  : Integer := 4;
-   Storage : List;
-   Access_Storage : Counting_Semaphore (1, Default_Ceiling);
-   Full_Storage   : Counting_Semaphore (StorageSize, Default_Ceiling);
-   Empty_Storage  : Counting_Semaphore (0, Default_Ceiling);
-   ProducersWorkDone : Integer := 0;
-   ConsumersWorkDone : Integer := 0;
-
+   procedure Init (ItemsTarget : in Integer; StorageSize : in Integer;
+                   ProducersCount : in Integer; ConsumersCount : in Integer) is
+      Storage : List;
+      Access_Storage : Counting_Semaphore (1, Default_Ceiling);
+      Full_Storage   : Counting_Semaphore (StorageSize, Default_Ceiling);
+      Empty_Storage  : Counting_Semaphore (0, Default_Ceiling);
+      ItemsProduced : Integer := 0;
+      ItemsReceived : Integer := 0;
+      Access_Get_ItemsProduced : Counting_Semaphore (1, Default_Ceiling);
+      Access_Get_ItemsReceived : Counting_Semaphore (1, Default_Ceiling);
 
       task type ProducerTask;
       task body ProducerTask is
       begin
 
-         while (ProducersWorkDone < WorkTarget) loop
+         Access_Get_ItemsProduced.Seize;
+         while (ItemsProduced < ItemsTarget) loop
+            ItemsProduced := ItemsProduced + 1;
+            Access_Get_ItemsProduced.Release;
 
             Full_Storage.Seize;
             Access_Storage.Seize;
 
-         if ProducersWorkDone >= WorkTarget then
-            Put_Line ("end of thread " & ProducersWorkDone'Img);
-            Full_Storage.Release;
-            Access_Storage.Release;
-            Empty_Storage.Release;
-            exit;
-            end if;
 
-            Storage.Append ("item " & ProducersWorkDone'Img);
-            Put_Line ("Producer add item " & ProducersWorkDone'Img);
-            ProducersWorkDone := ProducersWorkDone + 1;
+            Storage.Append ("item");
+            Put_Line ("Producer add item");
 
             Access_Storage.Release;
             Empty_Storage.Release;
 
          end loop;
-                Put_Line ("..........Producer was ended ");
       end ProducerTask;
 
 
@@ -55,31 +48,31 @@ procedure Main is
       task body ConsumerTask is
       begin
 
-         while (ConsumersWorkDone  < WorkTarget) loop
+         Access_Get_ItemsReceived.Seize;
+         while (ItemsReceived < ItemsTarget) loop
+            ItemsReceived := ItemsReceived + 1;
+            Access_Get_ItemsReceived.Release;
 
             Empty_Storage.Seize;
             Access_Storage.Seize;
 
-            if ConsumersWorkDone >= WorkTarget then
-            Full_Storage.Release;
-            Access_Storage.Release;
-            Empty_Storage.Release;
 
-            exit;
-            end if;
-               Put_Line ("Consumer took " & First_Element (Storage));
+            Put_Line ("Consumer took " & First_Element (Storage));
             Storage.Delete_First;
-            ConsumersWorkDone := ConsumersWorkDone + 1;
 
             Access_Storage.Release;
             Full_Storage.Release;
 
-      end loop;
-                  Put_Line ("..........Consumer was ended ");
+         end loop;
       end ConsumerTask;
 
       Consumers : Array(1..ConsumersCount) of ConsumerTask;
       Producers : Array(1..ProducersCount) of ProducerTask;
+
+   begin
+      null;
+   end Init;
+
 begin
-   null;
+   Init(10, 3, 6, 4);
 end Main;
